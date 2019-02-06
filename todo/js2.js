b96285a6ba6ff0446c32e23cf7140097799bb590
Alexey Dudnyk <45230390+Elektronik17@users.noreply.github.com>
2019-02-07 01:48:05 +0200

    todo pure JS

---
+++ todo/js2.js
@@ -0,0 +1,96 @@
+const todoForm = document.getElementById('todo-form');
+const addInput = document.getElementById('add-input');
+const todoList = document.getElementById('todo-list');
+const todoItems = document.querySelectorAll('.todo-item');
+
+todoForm.addEventListener('submit', addTodoItem);
+
+function addTodoItem(event) {
+    event.preventDefault();
+
+    if (addInput.value === '') return alert('Введите задачу');
+
+    const todoItem = createTodoItem(addInput.value); 
+    todoList.appendChild(todoItem);
+    addInput.value = '';
+}
+
+
+function createTodoItem(title) {
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'checkbox';
+
+    const label = document.createElement('label');
+    label.innerText = title;
+    label.className ='title';
+
+    const editInput = document.createElement('input');
+    editInput.type = 'text';
+    editInput.className = 'textfield';
+
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Изменить';
+    editButton.className = 'edit';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Удалить';
+    deleteButton.className = 'delete';
+
+    const listItem = document.createElement('li');
+    listItem.className = 'todo-item';
+
+    //запихаем все элементы в li
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+    listItem.appendChild(editInput);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+    //console.log(listItem);
+
+    bindEvents(listItem); //bindEv
+    return listItem;   
+}
+
+// bindEv
+function bindEvents(todoItem) {
+    const checkbox = todoItem.querySelector('.checkbox');
+    const editButton = todoItem.querySelector('button.edit');
+    const deleteButton = todoItem.querySelector('button.delete');
+
+    checkbox.addEventListener('change', toggleTodoItem); //1
+    editButton.addEventListener('click', editTodoItem); //2
+    deleteButton.addEventListener('click', deleteTodoItem); //3
+}
+
+//1
+function toggleTodoItem() {
+    const listItem = this.parentNode;
+    listItem.classList.toggle('completed');
+}
+
+//2
+function editTodoItem() {
+    const listItem = this.parentNode;
+    const title = listItem.querySelector('.title');
+    const editInput = listItem.querySelector('.textfield');
+    const isEditing = listItem.classList.contains('editing');
+
+    if (isEditing) {
+        title.innerText = editInput.value;
+        this.innerText = 'Изменить';
+    } else {
+        editInput.value = title.innerText;
+        this.innerText = 'Сохранить';
+    }
+
+    listItem.classList.toggle('editing');
+}
+
+//3
+function deleteTodoItem() {
+    const listItem = this.parentNode;
+    todoList.removeChild(listItem);
+}
+
+//continue
